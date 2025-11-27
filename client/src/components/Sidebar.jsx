644d11416a7e68c4dc908cda299e2a972dace468
2021331063 <2021331063@student.sust.edu>
2025-11-27 19:19:29 +0600

    Created folder and installed required dependencies for backend

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -50,7 +50,7 @@ const Sidebar = ({ sidebar, setSidebar }) => {
               className={({ isActive }) =>
                 `px-3.5 py-2.5 flex items-center gap-3 rounded ${
                   isActive
-                    ? "bg-gradient-to-r from-[#3C81F6] to-[#9234EA] text-white"
+                    ? "bg-linear-to-r from-[#3C81F6] to-[#9234EA] text-white"
                     : "text-gray-600"
                 }`
               }
