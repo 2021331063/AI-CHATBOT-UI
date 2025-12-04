a4675ba384fce6db546a48388a7271933cda9868
2021331063 <2021331063@student.sust.edu>
2025-12-04 21:17:45 +0600

    added a database and installed it using neon

--- server/server.js
+++ server/server.js
@@ -15,7 +15,6 @@ app.get("/", (req, res) => res.send("Server is Live!"));
 
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
 });
