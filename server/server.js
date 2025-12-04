5dfaa3084e8b91e3a055f389bf3cc2c340c69c60
2021331063 <2021331063@student.sust.edu>
2025-12-04 21:28:42 +0600

    Added user authentication

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import { clerkMiddleware, requireAuth } from "@clerk/express";
 
 
 const app = express();
@@ -8,9 +9,13 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(clerkMiddleware());
+
 
 
 app.get("/", (req, res) => res.send("Server is Live!"));
+
+app.use(requireAuth());
 
 
 
